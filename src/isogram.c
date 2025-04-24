dbc45e3a804a084225e348d4d00211d43a139732
Trigo09 <150361018+Trigo09@users.noreply.github.com>
2025-04-24 12:57:46 +0000

    add basic is_isogram implementation

--- src/isogram.c
+++ src/isogram.c
@@ -7,5 +7,13 @@
 
 bool is_isogram(const char phrase[]){
 
+    int characters[256] = {0};
+
+    for(int i=0; phrase[i] != '\0'; i++){
+        if(characters[phrase[i]])
+            return false;   // Character already seen
+        characters[phrase[i]] = 1;
+    }
+
     return true;
 }
